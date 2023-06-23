fb56680c9c9fa692e5280c0912ad30e15cfc3fb0
IG <aloneguid@outlook.com>
2023-06-23 19:05:55 +0100

    run pre-build as well

--- pre-build.ps1
+++ pre-build.ps1
@@ -1,6 +1,3 @@
-param([switch]$Blog = $false)
-
-
 $srcLog = ".\log.json"
 $log = Get-Content $srcLog | ConvertFrom-Json
 
@@ -9,13 +6,6 @@ $v1 = $version -replace "\.", ","
 $v1 += ",0"
 $v2 = $version + ".0"
 $cppLog = ".\bt\log.h"
-
-Write-Host "building version $version"
-
-# update version in globals.h
-(Get-Content .\bt\globals.h) `
-    -replace "Version\s*=\s*""(3.*)""", "Version = ""$version""" |
-    Out-File .\bt\globals.h
 
 $txtLog = ""
 $cppStructs = ""
@@ -93,18 +83,3 @@ const std::vector<change_version> ChangeLog {
 (Get-Content .\bt\bt.rc) `
     -replace "3\.\d+\.\d+\.\d+", "$v2" |
     Out-File .\bt\bt.rc
-
-# update version in cmake
-(Get-Content .\CMakeLists.txt) `
-    -replace "VERSION\s*(\d.\d.\d)", "VERSION $version" |
-    Out-File .\CMakeLists.txt
-
-# build executable
-$buildType = "Release"
-$triplet = "x64-windows-static" 
-$cconfig = "Release"
-iex "cmake -B build -S . -D CMAKE_BUILD_TYPE=$buildType -D CMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=$triplet"
-iex "cmake --build build --config $cconfig"
-cd build
-cpack -C $buildType
-cd ..
