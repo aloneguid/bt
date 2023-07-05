2aaa79077a3c430cfc4644a15c0291e850a5c380
IG <aloneguid@outlook.com>
2023-07-05 17:03:49 +0100

    replicated firefox extension

---
+++ browser-extensions/firefox/background.js
@@ -0,0 +1,30 @@
+
+browser.browserAction.onClicked.addListener((tab) => {
+    var url = tab.url;
+
+    console.log("clicked from toolbar: " + url);
+
+    browser.tabs.update(tab.id, {
+        url: "x-bt://" + url
+    });
+});
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+    var url = info.linkUrl;
+
+    console.log("clicked from context menu: " + url);
+
+    browser.tabs.update(tab.id, {
+        url: "x-bt://" + url
+    });
+});
+
+browser.runtime.onInstalled.addListener(() => {
+
+    browser.contextMenus.create({
+        id: "bt-menu-item",
+        title: "Open with Browser Tamer",
+        contexts: ["link"]
+    });
+
+});
