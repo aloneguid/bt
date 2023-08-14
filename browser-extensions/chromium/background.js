66ae301eecc7a882fee395892bc0fea71cf05f78
IG <aloneguid@outlook.com>
2023-08-14 22:13:24 +0100

    remove unused chrome permissions, bump extension version

--- browser-extensions/chromium/background.js
+++ browser-extensions/chromium/background.js
@@ -19,27 +19,6 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   openInBT(tab.id, info.linkUrl);
 });
 
-/*
-async function beforeNav(details){
-
-  let activeTabs = await chrome.tabs.query({currentWindow: true, active: true});
-  let tab = activeTabs[0];
-
-  // to read tab.url you need "tabs" permission
-
-  console.log("from:");
-  console.log(tab.url);
-  console.log("to:")
-  console.log(details);
-
-  // await chrome.tabs.update(details.tabId, {url: "http://microsoft.com"});
-
-  // openInBT(details.tabId, details.url);
-}
-
-chrome.webNavigation.onBeforeNavigate.addListener(beforeNav);
-*/
-
 // add context menu item for a hyperlink (contexts: link)
 chrome.runtime.onInstalled.addListener(() => {
 
