251a64cb32aeb416f0f4114d229cc5d190e3e879
IG <aloneguid@outlook.com>
2023-08-22 11:09:56 +0100

    is this failing?

--- bt/bt.cpp
+++ bt/bt.cpp
@@ -56,11 +56,7 @@ void execute(const string& data) {
 
 void CALLBACK KeepAliveTimerProc(HWND hwnd, UINT message, UINT_PTR idTimer, DWORD dwTime) {
     t.track(map<string, string> {
-        { "event", "ping" },
-        { "log_rule_hits", bt::config::i.get_log_rule_hits() ? "y" : "n" },
-        { "theme", bt::config::i.get_theme() },
-        { "unshort", bt::config::i.get_unshort_enabled() ? "y" : "n" },
-        { "browsers_total", std::to_string(bt::browser::get_cache().size()) }
+        { "event", "ping" }
     }, true);
 }
 
