da418ddbdd1dc799a88a1ee6e52f455ad486a1ce
justmrphoenix <mrphoenix@protonmail.com>
2025-02-27 17:30:29 +0400

    Rework gecko extension to support protocol approval

---
+++ browser-extensions/gecko/protocol_page.js
@@ -0,0 +1,14 @@
+﻿const BtProtoPrefix = "x-bt://";
+const url = window.location.hash.slice(1);
+
+console.log(`Navigating to ${BtProtoPrefix + url}`);
+
+window.location.href = BtProtoPrefix + url;
+
+document.addEventListener("visibilitychange", () => {
+    // Close the tab on any focus change
+    // User confirmed protocol => page is visible => closed
+    // User canceled protocol => nothing happens => page remains open
+    // Protocol was confirmed permanently => page is visible => closed
+    window.close()
+});
