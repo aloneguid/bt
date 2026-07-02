961933a7c5a56ad82247e1a326a3a8475bd6246c
aloneguid <aloneguid@outlook.com>
2026-07-02 12:44:34 +0100

    linux fixes

--- bt/app/url_opener.cpp
+++ bt/app/url_opener.cpp
@@ -1,6 +1,9 @@
 #include "url_opener.h"
 #include "url_pipeline.h"
 #include "../globals.h"
+#include "url.h"
+
+using namespace grey::common;
 
 namespace bt {
     void url_opener::open(std::shared_ptr<browser_instance> bi, click_payload up) {
@@ -14,13 +17,17 @@ namespace bt {
 
     void url_opener::open(click_payload up) {
         auto matches = browser::match(g_settings.browsers, up, g_settings.default_profile, g_script);
-        browser_match_result& first_match = matches[0];
-        up.app_mode = first_match.rule.app_mode;
-        open(first_match.bi, up);
+        if(matches.empty()) {
+            url::open_in_browser(up.url);;
+        } else {
+            browser_match_result& first_match = matches[0];
+            up.app_mode = first_match.rule.app_mode;
+            open(first_match.bi, up);
+        }
     }
 
     void url_opener::open(const std::string& url) {
-        click_payload up{ url };
+        const click_payload up{ url };
         open(up);
     }
 }
