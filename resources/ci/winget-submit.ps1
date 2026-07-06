e3451c1762fd8a94ac8a6984cca74a54c418e54f
aloneguid <aloneguid@outlook.com>
2026-07-06 10:09:49 +0100

    consolidate resource locations

--- resources/ci/winget-submit.ps1
+++ resources/ci/winget-submit.ps1
@@ -1,4 +1,4 @@
-$version = (Get-Content -Path .\docs\release-notes.md -First 1).Trim('#', ' ')
+$version = (Get-Content -Path ..\..\docs\release-notes.md -First 1).Trim('#', ' ')
 
 Write-Host "submiting version $($version)..."
 
