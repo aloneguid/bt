8acb5519e835330e310eadf0683ec2337ec1a1c9
aloneguid <aloneguid@outlook.com>
2026-08-06 10:27:54 +0100

    version update

--- bt/globals.h
+++ bt/globals.h
@@ -44,7 +44,7 @@
 
 #define APP_REG_DESCRIPTION "Redirects open URLs to a browser of your choice."
 
-#define APP_VERSION "6.0.2"
+#define APP_VERSION "6.1.0"
 
 extern bt::url_pipeline g_pipeline;
 
